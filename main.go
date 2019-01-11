c480d9496c2a7378c6154ca834d16974aa8fd656
Seshachalam Malisetti <abbiya@gmail.com>
2019-01-12 04:14:20 +0530

    reformating and fixes

--- main.go
+++ main.go
@@ -11,13 +11,16 @@ import (
 	"os/signal"
 	"sync"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 const (
-	topStories  = "https://hacker-news.firebaseio.com/v0/topstories.json"
-	bestStories = "https://hacker-news.firebaseio.com/v0/beststories.json"
-	storyLink   = "https://hacker-news.firebaseio.com/v0/item/%d.json"
-	hnPostLink  = "https://news.ycombinator.com/item?id=%d"
+	topStories           = "https://hacker-news.firebaseio.com/v0/topstories.json"
+	bestStories          = "https://hacker-news.firebaseio.com/v0/beststories.json"
+	storyLink            = "https://hacker-news.firebaseio.com/v0/item/%d.json"
+	hnPostLink           = "https://news.ycombinator.com/item?id=%d"
+	frontPageNumArticles = 30
 )
 
 type limitMap struct {
@@ -77,158 +80,240 @@ type stories struct {
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	stop := make(chan os.Signal, 1)
-
-	signal.Notify(stop, os.Interrupt)
-
 	tmpl := template.New("index.html")
 	tmpl, err := tmpl.ParseFiles("./index.html")
 	if err != nil {
 		panic(err)
 	}
+
+	errCh := make(chan error)
+	defer close(errCh)
+
 	st := stories{
 		list: make(map[int]item),
 	}
-
-	ticker := time.NewTicker(5 * time.Minute)
-	defer ticker.Stop()
 
 	storiesURLs := []string{topStories}
 	incomingItems := make(chan itemList)
 	defer close(incomingItems)
 
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, os.Interrupt, os.Kill)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	const trackingLimit = 2000
 	visited := make(map[int]struct{})
-	lm := newLM(visited, 2000) // limit the tracking to 2000 items
+	lm := newLM(visited, trackingLimit)
 
-	fetchStories := func() {
-		var wg sync.WaitGroup
+	tickerTicker := func(ctx context.Context, tickers []*time.Ticker, calls []func(ctx context.Context) error) error {
+		var eg errgroup.Group
+		for i, tckr := range tickers {
+			i := i
+			tckr := tckr
+			eg.Go(func() error {
+				<-tckr.C
+				return calls[i](ctx)
+			})
+		}
+
+		return eg.Wait()
+	}
+
+	topStoriesFetcher := func(limit int) error {
+		var eg errgroup.Group
 		for _, storiesURL := range storiesURLs {
-			wg.Add(1)
-			go func(storiesURL string) {
-				defer wg.Done()
-				resp, err := http.Get(storiesURL)
-				if err != nil {
-					log.Println(err)
-					return
-				}
+			storiesURL := storiesURL
+			eg.Go(func() error {
+				fetchStories := func(limit int) ([]int, error) {
+					resp, err := http.Get(storiesURL)
+					if err != nil {
+						return nil, err
+					}
 
-				defer resp.Body.Close()
+					defer resp.Body.Close()
 
-				decoder := json.NewDecoder(resp.Body)
-				var items itemList
-				err = decoder.Decode(&items)
-				if err != nil {
-					log.Println(err)
-					return
+					decoder := json.NewDecoder(resp.Body)
+					var items itemList
+					err = decoder.Decode(&items)
+					if err != nil {
+						return nil, err
+					}
+					if len(items) < limit {
+						limit = len(items)
+					}
+					return items[:limit], nil
 				}
 
 				// send items
-				incomingItems <- items[:30] // homepage shows only 30
-			}(storiesURL)
+				items, err := fetchStories(limit)
+				if err != nil {
+					return err
+				}
+				incomingItems <- items
+				return nil
+			})
 		}
-		wg.Wait()
+
+		return eg.Wait()
 	}
 
-	go fetchStories()
-
-	storyFetcher := func(ctx context.Context) {
-		for {
-			select {
-			case <-ticker.C:
-				fetchStories()
-			case <-ctx.Done():
-				return
-			}
+	fetchItem := func(itemID int) (i item, err error) {
+		resp, err := http.Get(fmt.Sprintf(storyLink, itemID))
+		if err != nil {
+			return
 		}
+
+		defer resp.Body.Close()
+
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&i)
+		if err != nil {
+			return
+		}
+
+		return i, nil
 	}
 
-	storyLister := func(ctx context.Context) {
+	storyLister := func(ctx context.Context) error {
 		for {
 			select {
 			case items := <-incomingItems:
 				for _, itemID := range items {
-					func() {
-						if _, ok := st.list[itemID]; ok {
-							return
-						}
+					select {
+					case <-ctx.Done():
+						return nil
+					default:
+						func() {
+							if _, ok := st.list[itemID]; ok {
+								return
+							}
 
-						if lm.has(itemID) {
-							return
-						}
+							if lm.has(itemID) {
+								return
+							}
 
-						lm.insert(itemID)
+							lm.insert(itemID)
 
-						item, err := fetchItem(itemID)
-						if err != nil {
-							log.Println(err)
-							return
-						}
-						item.Added = unixTime(time.Now().Unix())
+							item, err := fetchItem(itemID)
+							if err != nil {
+								errCh <- err
+								return
+							}
+							item.Added = unixTime(time.Now().Unix())
 
-						st.Lock()
-						defer st.Unlock()
-						st.list[itemID] = item
-					}()
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	}
-
-	ticker2 := time.NewTicker(5 * time.Minute)
-	defer ticker2.Stop()
-
-	storyRemover := func(ctx context.Context) {
-		for {
-			select {
-			case <-ticker2.C:
-				for id, it := range st.list {
-					if time.Since(time.Unix(int64(it.Added), 0)).Hours() > 8 {
-						st.Lock()
-						delete(st.list, id)
-						st.Unlock()
+							st.Lock()
+							defer st.Unlock()
+							st.list[itemID] = item
+						}()
 					}
 				}
 			case <-ctx.Done():
-				return
+				return nil
 			}
 		}
 	}
 
-	go storyFetcher(ctx)
-	go storyLister(ctx)
-	go storyRemover(ctx)
+	storyRemover := func() error {
+		for id, it := range st.list {
+			if time.Since(time.Unix(int64(it.Added), 0)).Seconds() > 8*60*60 {
+				st.Lock()
+				delete(st.list, id)
+				st.Unlock()
+			}
+		}
+		return nil
+	}
+
+	const hnPollTime = 5 * time.Minute
+	fiveMinTicker := time.NewTicker(hnPollTime)
+	defer fiveMinTicker.Stop()
+
+	listCounter := func() error {
+		st.Lock()
+		defer st.Unlock()
+		log.Println(st.list)
+		return nil
+	}
+
+	tickers := []*time.Ticker{fiveMinTicker}
+	calls := []func(ctx context.Context) error{
+		func(ctx context.Context) error {
+			var eg errgroup.Group
+			eg.Go(func() error {
+				log.Println("starting top stories fetcher")
+				return topStoriesFetcher(frontPageNumArticles)
+			})
+			eg.Go(func() error {
+				log.Println("starting story remover")
+				return storyRemover()
+			})
+			eg.Go(func() error {
+				log.Println("starting list counter")
+				return listCounter()
+			})
+			return eg.Wait()
+		},
+	}
+
+	errLogger := func(ctx context.Context) error {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case err := <-errCh:
+				log.Println(err)
+			}
+		}
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		st.Lock()
+		defer st.Unlock()
 		err = tmpl.Execute(w, st.list)
 		if err != nil {
-			log.Println(err)
+			errCh <- err
 		}
 		return
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("START")
+	log.Println("starting the app")
+	var eg errgroup.Group
+	eg.Go(func() error {
+		log.Println("starting error logger")
+		return errLogger(ctx)
+	})
+	eg.Go(func() error {
+		log.Println("starting top stories fetcher")
+		return topStoriesFetcher(frontPageNumArticles)
+	})
+	eg.Go(func() error {
+		time.Sleep(1 * time.Minute)
+		return listCounter()
+	})
+	eg.Go(func() error {
+		log.Println("starting story lister")
+		return storyLister(ctx)
+	})
+	eg.Go(func() error {
+		log.Println("starting ticker ticker")
+		return tickerTicker(ctx, tickers, calls)
+	})
+	eg.Go(func() error {
+		log.Println("starting http server on port 8080")
+		return http.ListenAndServe(":8080", nil)
+	})
+	eg.Go(func() error {
+		sig := <-stop
+		cancel()
+		return fmt.Errorf("interrupted with signal %s, aborting", sig.String())
+	})
 
-	<-stop
-	cancel()
-}
-
-func fetchItem(itemID int) (i item, err error) {
-	resp, err := http.Get(fmt.Sprintf(storyLink, itemID))
+	err = eg.Wait()
 	if err != nil {
-		return
+		log.Println(err)
 	}
-
-	defer resp.Body.Close()
-
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&i)
-	if err != nil {
-		return
-	}
-
-	return i, nil
+	log.Println("END")
 }
